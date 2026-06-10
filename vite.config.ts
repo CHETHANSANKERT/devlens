171e37cac1baf9c0e74c9469035984a48338e45b
chethansankert <chethansankert@gmail.com>
2026-06-10 20:36:31 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ function figmaAssetResolver() {
 
 export default defineConfig({
   // Hosted on: https://<user>.github.io/devlens/
-  base: '/devlens/',
+  base: '/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
